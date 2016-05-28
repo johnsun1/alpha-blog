e8b8448c4203a39949f033115665794950d9fb62
John Sun <john.sun@sjsu.edu>
2016-05-28 18:49:04 +0000

    Installed Bootstrap gem.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,7 +19,6 @@ class ArticlesController < ApplicationController
     end
     
     def update
-        
         if @article.update(article_params)
             flash[:notice] = "Article was successfully updated."
             redirect_to article_path(@article)
