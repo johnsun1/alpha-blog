6b2edb8e3dd32602178fa65f8bb73e111c4fb6bd
John Sun <john.sun@sjsu.edu>
2016-05-29 18:12:10 +0000

    Updated length restrictions for title and body of articles.

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,7 @@ class Article < ActiveRecord::Base
   belongs_to :user
   has_many :article_categories
   has_many :categories, through: :article_categories
-  validates :title, presence: true, length: { minimum: 3, maximum: 50 }
-  validates :description, presence: true, length: { minumum: 10, maximum: 300 }
+  validates :title, presence: true, length: { minimum: 3, maximum: 100 }
+  validates :description, presence: true, length: { minumum: 10, maximum: 100000 }
   validates :user_id, presence: true
 end
