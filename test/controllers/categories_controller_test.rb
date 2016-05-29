b244c93c80e821bd23144a80afc2737b276a45df
John Sun <john.sun@sjsu.edu>
2016-05-29 16:13:18 +0000

    Added more automated testing and updated navbar with categories.

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -1,9 +1,10 @@
 require 'test_helper'
 
 class CategoriesControllerTest < ActionController::TestCase
-    def setup
-       @category = Category.create(name: "sports") 
-    end
+  def setup
+    @category = Category.create(name: "sports") 
+    @user = User.create(username: "John", email: "john@example.com", password: "Kappa", admin: true)
+  end
     
   test "should get categories index" do
     get :index
@@ -11,6 +12,7 @@ class CategoriesControllerTest < ActionController::TestCase
   end
   
   test "should get new" do
+      session[:user_id] = @user.id
       get :new
       assert_response :success
   end
@@ -19,4 +21,12 @@ class CategoriesControllerTest < ActionController::TestCase
      get(:show, { 'id' => @category.id })
      assert_response :success
   end
+  
+  test "should redirect create when not signed in as admin" do
+    assert_no_difference 'Category.count' do
+        post :create, category: { name: "sports" }
+    end
+    assert_redirected_to categories_path
+  end
+  
 end
