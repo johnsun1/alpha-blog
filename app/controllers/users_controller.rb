3ebb8c133093e473c64bcf36279834008b0c0c87
John Sun <john.sun@sjsu.edu>
2016-05-29 04:52:03 +0000

    Implemented admin user powers.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
    before_action :set_user, only: [:edit, :update, :show]
-   before_action :require_same_user, only: [:edit, :update]
+   before_action :require_same_user, only: [:edit, :update, :destroy]
+   before_action :require_admin, only: [:destroy]
    
    def new
       @user = User.new 
@@ -34,6 +35,13 @@ class UsersController < ApplicationController
       @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
    end
    
+   def destroy
+      @user = User.find(params[:id])
+      @user.destroy
+      flash[:danger] = "User (and all articles) have been deleted."
+      redirect_to users_path
+   end
+   
    def index
       @users = User.paginate(page: params[:page], per_page: 5)
    end
@@ -48,8 +56,15 @@ class UsersController < ApplicationController
    end
    
    def require_same_user
-      if !logged_in? && current_user != @user
+      if !logged_in? && current_user != @user && !current_user.admin?
          flash[:danger] = "You can only edit your own account."
+         redirect_to root_path
+      end
+   end
+   
+   def require_admin
+      if logged_in? && !current_user.admin?
+         flash[:danger] = "Only admins may perform that action."
          redirect_to root_path
       end
    end
